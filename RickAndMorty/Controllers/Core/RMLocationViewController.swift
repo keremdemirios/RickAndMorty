155b7f80339c3f3f4595b3fbb6b9aa500795a428
Kerem Demır <keremdemir@Kerems-MacBook-Air.local>
2023-03-30 11:25:11 +0300

    Created Rick and Morty project.

---
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RMLocationViewController.swift
+//  RickAndMorty
+//
+//  Created by Kerem Demır on 30.03.2023.
+//
+
+import UIKit
+
+final class RMLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Locations"
+        
+    }
+
+}
---
+++ RickAndMorty/Models/RMCharacters.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacters.swift
+//  RickAndMorty
+//
+//  Created by Kerem Demır on 30.03.2023.
+//
+
+import Foundation
